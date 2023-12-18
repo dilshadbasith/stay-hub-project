de7f61c5d3dd4e2da91023327dc85242cd5d02e1
dilshadbasith <dilshadbasith01@gmail.com>
2023-12-18 14:17:04 -0800

    add footer

--- src/Components/Cards/BrowseCards.jsx
+++ src/Components/Cards/BrowseCards.jsx
@@ -2,22 +2,35 @@ import React, { useContext } from "react";
 import { myContext } from "../Context";
 import { useParams } from "react-router-dom";
 import Navigationbar from "../Header/Navigationbar";
+import "../Cards/Cards.css";
 
 function BrowseCards() {
   const { cards } = useContext(myContext);
   const { id } = useParams();
 
+  
+
   const data = cards.filter((item) => item.id === parseInt(id));
   return (
     <div>
       <Navigationbar />
-      {data.map((item,index) => (
-        <div key={index}>
-            <h2>{item.title}</h2>
-            <img src={item.imgSrc[0]} alt="" />
-                                
+      {data.map((item, index) => (
+        <div key={index} className="browse-div">
+          <h2 className="browse-title">{item.desc}</h2>
+          <div className="img-main-div">
+            <div>
+              <img src={item.imgSrc[0]} alt="image" className="img1" />
+            </div>
+            <div className="img-sub-div">
+              <img src={item.imgSrc[1]} alt="image" className="img2" />
+              <img src={item.imgSrc[2]} alt="image" className="img2" />
+            </div>
+            <div className="img-sub-div">
+              <img src={item.imgSrc[3]} alt="image" className="img2" />
+              <img src={item.imgSrc[4]} alt="image" className="img2" />
+            </div>
+          </div>
         </div>
-        
       ))}
     </div>
   );
