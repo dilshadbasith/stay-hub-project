346608995d999b704c0b25c26f447b0eb6cfd2cc
dilshadbasith <dilshadbasith01@gmail.com>
2023-12-22 14:58:39 -0800

    finished formik and yup

--- src/Components/Modals/RegisterModal.jsx
+++ src/Components/Modals/RegisterModal.jsx
@@ -12,27 +12,30 @@ import {
 import { myContext } from "../Context";
 import { useFormik } from "formik";
 import { basicSchema } from "./RegisterSchema";
+import "./LoginModal.css";
 
-const onSubmit = () =>{
-  console.log("submitted")
-}
+const onSubmit = (values,actions) => {
+  console.log(values);
+  console.log(actions)
+};
 
 export default function DialogWithForm() {
   const { handleOpen } = useContext(myContext);
 
-  const {values,errors,handleBlur,handleChange,handleSubmit} = useFormik({
-    initialValues: {
-      name: "",
-      email: "",
-      mobile: "",
-      username: "",
-      password: "",
-      confirmpassword: "",
-    },
-    validationSchema:basicSchema,
-    onSubmit,
-  });
-  console.log(errors)
+  const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
+    useFormik({
+      initialValues: {
+        name: "",
+        email: "",
+        mobile: "",
+        username: "",
+        password: "",
+        confirmpassword: "",
+      },
+      validationSchema: basicSchema,
+      onSubmit,
+    });
+  console.log(errors);
 
   return (
     <>
@@ -44,7 +47,7 @@ export default function DialogWithForm() {
           className="bg-transparent shadow-none"
         >
           <Card className="mx-auto w-full max-w-[24rem]">
-            <CardBody className="flex flex-col gap-4">
+            <CardBody className="flex flex-col gap-4 max-h-modal-body overflow-auto">
               <Typography variant="h4" color="red" className="text-center">
                 Register
               </Typography>
@@ -56,9 +59,9 @@ export default function DialogWithForm() {
                 size="lg"
                 value={values.name}
                 onChange={handleChange}
-                onBlur={handleBlur}
-                className={name?"input-error":""}
+                onBlur={handleBlur}                
               />
+              {errors.name && touched.name && <p className="error">{errors.name}</p>}
               <Input
                 label="Email"
                 type="email"
@@ -68,6 +71,7 @@ export default function DialogWithForm() {
                 onChange={handleChange}
                 onBlur={handleBlur}
               />
+              {errors.email && touched.email && <p className="error">{errors.email}</p>}
               <Input
                 label="Mobile"
                 size="lg"
@@ -77,6 +81,7 @@ export default function DialogWithForm() {
                 onChange={handleChange}
                 onBlur={handleBlur}
               />
+              {errors.mobile && touched.mobile && <p className="error">{errors.mobile}</p>}
               <Input
                 label="Username"
                 size="lg"
@@ -86,6 +91,7 @@ export default function DialogWithForm() {
                 onChange={handleChange}
                 onBlur={handleBlur}
               />
+              {errors.username && touched.username && <p className="error">{errors.username}</p>}
               <Input
                 label="Password"
                 size="lg"
@@ -95,6 +101,7 @@ export default function DialogWithForm() {
                 onChange={handleChange}
                 onBlur={handleBlur}
               />
+              {errors.password && touched.password && <p className="error">{errors.password}</p>}
               <Input
                 label="Confirm Password"
                 size="lg"
@@ -104,9 +111,10 @@ export default function DialogWithForm() {
                 onChange={handleChange}
                 onBlur={handleBlur}
               />
-              <div className="-ml-2.5 -mt-3">
+              {errors.confirmpassword && touched.confirmpassword && <p className="error">{errors.confirmpassword}</p>}
+              {/* <div className="-ml-2.5 -mt-3">
                 <Checkbox label="Remember Me" />
-              </div>
+              </div> */}
             </CardBody>
             <CardFooter className="pt-0">
               <Button
